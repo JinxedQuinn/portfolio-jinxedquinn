f04757bb0dda5934fda835e1d411bde521b1c26b
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-05-23 12:02:36 +0200

    Intégration Projets Portfolio

--- app/_components/SideProjects.tsx
+++ app/_components/SideProjects.tsx
@@ -1,52 +1,80 @@
 import Link from "next/link";
 import {
-  ALargeSmall, Code,
+  Beer,
+  ChefHat,
+  CircleFadingPlus,
+  Coins,
+  Gamepad,
+  Home,
+  Inbox,
   LucideIcon,
-  MessageCircle,
   Rss,
-  StickyNote,
-  Weight
-} from "lucide-react";
+  Smartphone,
+  Trophy} from "lucide-react";
 
-/* Projets */
 export const SIDE_PROJECTS: SideProjectsProps[] = [
   {
-    Logo: Code,
-    title: "Codeline",
-    description: "Mon site on va dire",
+    Logo: Trophy,
+    title: "Pride Score",
+    description: "Site web footbalistique répertoriant toutes les compétitions de football amateures féminines non affiliées à la FFF. A venir",
     url: "/",
   },
   {
-    Logo: StickyNote,
-    title: "PowerNote",
-    description: "Le deuxieme projet utilisé",
-    url: "/",
+    Logo: Smartphone,
+    title: "Projet Mobile",
+    description: "Projet Mobile sur React Native et Go",
+    url: "https://github.com/adatechschool/projet-collectif-mobile-ACG",
   },
   {
     Logo: Rss,
-    title: "PowerPost",
-    description: "Lorem Ipsum et la situation et blablabla",
-    url: "/",
+    title: "Projet Microblogging",
+    description: "Projet Microblogging sur Docker et Laravel",
+    url: "https://github.com/adatechschool/projet-collectif-microblogging-clement-chloe-chloe-tom",
   },
   {
-    Logo: MessageCircle,
-    title: "Chat2Code",
-    description: "Lorem Ipsum et la situation et blablabla",
-    url: "/",
+    Logo: ChefHat,
+    title: "Le Fou Frog",
+    description: "Projet de site web pour un restaurant français aux États-Unis. Codé avec Ryo Rafael, propriétaire du projet",
+    url: "https://github.com/ryorafael/lefoufrog",
   },
   {
-    Logo: Weight,
-    title: "LinkedLeads",
-    description: "A 222 platform blabla blablablebble",
-    url: "/",
+    Logo: Beer,
+    title: "La Mutinerie",
+    description: "J'ai choisi de mon plein gré, de refaire le site de la Mutinerie",
+    url: "https://github.com/JinxedQuinn/lamutinerie",
   },
   {
-    Logo: ALargeSmall,
-    title: "BulkCorrect",
-    description: "A 222 platform blabla blablablebble",
-    url: "/",
+    Logo: Home,
+    title: "Projet Meuble",
+    description: "Une plateforme de vente de meuble, avec le front et le back",
+    url: "https://github.com/JinxedQuinn/projetMeuble",
+  },
+{
+    Logo: CircleFadingPlus,
+    title: "Send",
+    description: "Un réseau social prenant comme modèle Twitter, codé en PHP",
+    url: "https://github.com/adatechschool/reseau-social-php-chacsocialnetwork?tab=readme-ov-file",
+  },
+{
+    Logo: Coins,
+    title: "Extension Navigateur",
+    description: "Une extension navigateur qui sert à calculer le taux de change monétaire",
+    url: "https://github.com/adatechschool/extension-navigateur-extentionnavigateur-cgr",
+  },
+{
+    Logo: Gamepad,
+    title: "Pokemon Project",
+    description: "Un Pokédex avec utilisation de l'API Pokémon Officielle",
+    url: "https://github.com/Stoyots/pokemonFinal",
+  },
+{
+    Logo: Inbox,
+    title: "AdaBox : Boîte à suggestions",
+    description: "Une boîte à suggestions pour que les étudiant.e.s puissent s'exprimer anonymement",
+    url: "https://github.com/JinxedQuinn/AdaBox",
   },
 ];
+
 type SideProjectsProps = {
   Logo: LucideIcon;
   title: string;
